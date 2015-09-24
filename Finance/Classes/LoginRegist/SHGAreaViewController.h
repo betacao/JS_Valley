b61f1e9e40cfbe28c9e593759c71796c9f6f17c5
曹长喜 <941038274@qq.com>
2015-09-24 17:07:19 +0800

    添加了地址位置plist

---
+++ Finance/Classes/LoginRegist/SHGAreaViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHGAreaViewController.h
+//  Finance
+//
+//  Created by changxicao on 15/9/24.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHGAreaViewController : UITableViewController
+
+@end
