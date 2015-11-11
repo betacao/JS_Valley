b6a45c08f380f79dbcffdedd7c7bad236ebfe68a
曹长喜 <941038274@qq.com>
2015-11-11 10:11:54 +0800

    新建个人动态界面

---
+++ Finance/Classes/Circle/Controllers/SHGPersonalViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHGPersonalViewController.h
+//  Finance
+//
+//  Created by changxicao on 15/11/11.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SHGPersonalViewController : BaseViewController
+
+@end
