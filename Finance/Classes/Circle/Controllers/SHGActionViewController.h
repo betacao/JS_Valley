b986f95d80a690a0a950c07790387bd2ffdb9e64
魏虔坤 <820505758@qq.com>
2015-11-12 16:19:10 +0800

    添加活动ViewController code review by 小鱼

---
+++ Finance/Classes/Circle/Controllers/SHGActionViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHGActionViewController.h
+//  Finance
+//
+//  Created by 魏虔坤 on 15/11/12.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+@interface SHGActionViewController : BaseTableViewController
+
+@end
