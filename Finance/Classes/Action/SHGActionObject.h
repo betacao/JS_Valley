edbca5b4df535b61213ec9087dc23218bdd39a25
曹长喜 <941038274@qq.com>
2015-11-13 13:42:09 +0800

    添加活动对象

---
+++ Finance/Classes/Action/SHGActionObject.h
@@ -0,0 +1,13 @@
+//
+//  SHGActionObject.h
+//  Finance
+//
+//  Created by changxicao on 15/11/13.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import <Mantle/Mantle.h>
+
+@interface SHGActionObject : MTLModel
+
+@end
---
+++ Finance/Classes/Action/SHGActionObject.m
@@ -0,0 +1,13 @@
+//
+//  SHGActionObject.m
+//  Finance
+//
+//  Created by changxicao on 15/11/13.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import "SHGActionObject.h"
+
+@implementation SHGActionObject
+
+@end
