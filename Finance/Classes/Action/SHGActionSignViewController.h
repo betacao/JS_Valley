79c10eb10949da1c7dc0873bc9ccb50f18e059de
魏虔坤 <820505758@qq.com>
2015-11-13 14:39:54 +0800

    添加报名的viewCOntroller  code review by 小鱼

---
+++ Finance/Classes/Action/SHGActionSignViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHGActionSignViewController.h
+//  Finance
+//
+//  Created by 魏虔坤 on 15/11/13.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SHGActionSignViewController : BaseViewController
+
+@end
