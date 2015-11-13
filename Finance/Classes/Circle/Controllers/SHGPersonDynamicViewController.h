b41a8d3a08fad759fcbefdd9c49a2185c94dfa21
曹长喜 <941038274@qq.com>
2015-11-13 10:44:50 +0800

    添加个人信息列表界面

---
+++ Finance/Classes/Circle/Controllers/SHGPersonDynamicViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHGPersonDynamicViewController.h
+//  Finance
+//
+//  Created by changxicao on 15/11/13.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+@interface SHGPersonDynamicViewController : BaseTableViewController
+
+@end
---
+++ Finance/Classes/Circle/Controllers/SHGPersonDynamicViewController.m
@@ -0,0 +1,37 @@
+//
+//  SHGPersonDynamicViewController.m
+//  Finance
+//
+//  Created by changxicao on 15/11/13.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import "SHGPersonDynamicViewController.h"
+
+@interface SHGPersonDynamicViewController ()
+
+@end
+
+@implementation SHGPersonDynamicViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
