3a20fcb066a3161e8c0c14970e2af4f7ac3679be
魏虔坤 <820505758@qq.com>
2015-11-13 14:22:00 +0800

    添加报名的cell code review by 小鱼

---
+++ Finance/Classes/Action/SGHActionSignTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SGHActionSignTableViewCell.h
+//  Finance
+//
+//  Created by 魏虔坤 on 15/11/13.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGHActionSignTableViewCell : UITableViewCell
+
+@end
---
+++ Finance/Classes/Action/SGHActionSignTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SGHActionSignTableViewCell.m
+//  Finance
+//
+//  Created by 魏虔坤 on 15/11/13.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import "SGHActionSignTableViewCell.h"
+
+@implementation SGHActionSignTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
