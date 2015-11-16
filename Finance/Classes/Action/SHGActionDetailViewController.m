6261ee4afd049e97b970858af80710bd677da907
魏虔坤 <820505758@qq.com>
2015-11-16 20:44:27 +0800

    修改活动详情界面 code review by 小鱼

--- Finance/Classes/Action/SHGActionDetailViewController.m
+++ Finance/Classes/Action/SHGActionDetailViewController.m
@@ -8,12 +8,19 @@
 
 #import "SHGActionDetailViewController.h"
 #import "ReplyTableViewCell.h"
-
+#import "SHGActionSignTableViewCell.h"
+#define k_title_height 44.0
+#define k_redLineToLeft 14;
+#define k_labelToLeft 20;
+#define k_imageToLabel 
 //#import "SHGActionTableViewCell.h"
 @interface SHGActionDetailViewController ()<UITableViewDataSource,UITableViewDelegate>
-
+{
+    //UITableView * ybmTableView;
+}
+@property (strong,nonatomic)UITableView *ybmTableView;
 @property (weak, nonatomic) IBOutlet UITableView *replyTable;
-@property (strong, nonatomic) IBOutlet UIView *viewHeader;
+@property (weak, nonatomic) IBOutlet UIView *viewHeader;
 @property (weak, nonatomic) IBOutlet UIImageView *titleBg;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UIView *JBXXLine;
@@ -72,15 +79,62 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     self.title = @"活动详情";
     self.replyTable.delegate = self;
     self.replyTable.dataSource = self;
-    //self.replyTable.backgroundColor = [UIColor redColor];
-    //self.viewHeader.backgroundColor = [UIColor redColor];
-    //_viewHeader.height = 5000.0;
-    self.replyTable.frame = CGRectMake(0, 0, [[UIScreen mainScreen]bounds].size.width, [[UIScreen mainScreen]bounds].size.height-50);
+        
+    [self setColor];
+    
+    [self setImage];
+    
+    [self setFram];
+   
+}
+-(void)setFram
+{
+    NSInteger num = 3;
+    NSInteger  plRowHeight = 54.0;
+    self.replyTable.frame = CGRectMake(0, 0, [[UIScreen mainScreen]bounds].size.width, [[UIScreen mainScreen]bounds].size.height);
     self.titleBg.frame = CGRectMake(0,0 , self.replyTable.frame.size.width, 44);
     self.titleLabel.frame = CGRectMake(0,0 , self.replyTable.frame.size.width, 44);
+    self.jjMessageLabel.numberOfLines = 0;//任意行数
+    NSString  *text = @"xxxx文件  来哈市办法V大搜啊；哈水电费；的；撒谎和；顺丰发大水了那副；厉害多撒谎；好；HDL；觳觫；发挥；萨满皮肤撒谎封IP撒谎覅哈斯；换肤dsa";
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:text];
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    [paragraphStyle setLineSpacing:1.f];//调整行间距
+    [attributedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(0, [text length])];
+    self.jjMessageLabel.attributedText = attributedString;
+    CGSize size = [self.jjMessageLabel sizeThatFits:CGSizeMake(self.replyTable.frame.size.width-40.0, MAXFLOAT)];
+    CGRect frame = self.jjMessageLabel.frame;
+    
+    frame.size.height = size.height;
+    self.jjMessageLabel.frame = CGRectMake(20, self.jjLabel.frame.origin.y+35, self.replyTable.frame.size.width-40, frame.size.height);
+    self.CenterLineImage.frame = CGRectMake(0,self.jjMessageLabel.frame.size.height+self.jjMessageLabel.frame.origin.y+15 , self.replyTable.frame.size.width, 1);
+    self.topLineImage.frame = CGRectMake(0, self.bmLabel.frame.size.height+self.bmLabel.frame.origin.y +15, [UIScreen mainScreen].bounds.size.width, 1);
+    self.yBMRedLine.frame = CGRectMake(14, self.CenterLineImage.frame.origin.y+9, 1, 15);
+    self.yBMLabel.frame = CGRectMake(25, self.CenterLineImage.frame.origin.y+6, 54, 20);
+    self.yBMNumLabel.frame = CGRectMake(87, self.CenterLineImage.frame.origin.y+6, 54, 20);
+    
+    self.commentView.hidden = YES;
+    _ybmTableView = [[UITableView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(self.yBMLabel.frame), self.replyTable.frame.size.width, plRowHeight *num)];
+    self.ybmTableView.delegate = self;
+    self.ybmTableView.dataSource = self;
+    self.ybmTableView.scrollEnabled = NO;
+    self.ybmTableView.showsHorizontalScrollIndicator = NO;
+    self.ybmTableView.showsVerticalScrollIndicator = NO;
+    self.ybmTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    [self.viewHeader addSubview:self.ybmTableView];
+    
+    self.checkButton.frame = CGRectMake(self.replyTable.frame.size.width-14-20 , self.CenterLineImage.frame.origin.y+11, 10, 10);
+    self.checkLabel.frame = CGRectMake(self.replyTable.frame.size.width-14-20-5-60, self.CenterLineImage.frame.origin.y+6, 60, 20);
+    self.plbutton.frame = CGRectMake(self.replyTable.frame.size.width-14-50, CGRectGetMaxY(self.ybmTableView.frame)+10, 50, 20);
+    self.zanButton.frame = CGRectMake(self.replyTable.frame.size.width-14-50-50, CGRectGetMaxY(self.ybmTableView.frame)+10, 50, 20);
+    self.bottomLine.frame = CGRectMake(0, self.zanButton.frame.origin.y-6, self.replyTable.frame.size.width, 1);
+    self.viewHeader.height = self.zanButton.frame.origin.y + self.zanButton.frame.size.height +10;
+}
+-(void)setColor
+{
     self.timeLabel.backgroundColor = [UIColor clearColor];
     self.timeLabel.textColor = [UIColor colorWithHexString:@"3A3A3A3A"];
     self.JBXXLine.backgroundColor = [UIColor colorWithHexString:@"F95C53"];
@@ -103,7 +157,9 @@
     self.firstCommpany.textColor = [UIColor colorWithHexString:@"606060"];
     self.secondCommpany.textColor = [UIColor colorWithHexString:@"606060"];
     self.thirCommpany.textColor = [UIColor colorWithHexString:@"606060"];
-    
+}
+-(void)setImage
+{
     [self.zanButton setImage:[UIImage imageNamed:@"home_weizan"] forState:UIControlStateNormal];
     [self.zanButton setTitle:@"555" forState:UIControlStateNormal];
     [self.zanButton setTitleColor:[UIColor colorWithHexString:@"D1D1D1"] forState:UIControlStateNormal];
@@ -116,71 +172,71 @@
     self.topLineImage.image = [img resizableImageWithCapInsets:UIEdgeInsetsMake(0 , 1, 0, 1) resizingMode:UIImageResizingModeTile];
     self.CenterLineImage.image = [img resizableImageWithCapInsets:UIEdgeInsetsMake(0 , 1, 0, 1) resizingMode:UIImageResizingModeTile];
     self.bottomLine.image = [img resizableImageWithCapInsets:UIEdgeInsetsMake(0 , 1, 0, 1) resizingMode:UIImageResizingModeTile];
-    
-   
-    self.jjMessageLabel.numberOfLines = 0;//任意行数
-    NSString  *text = @"xxxx文件  来哈市办法V大搜啊；哈水电费；的；撒谎和；顺丰发大水了那副；厉害多撒谎；好；HDL；觳觫；发挥；萨满皮肤撒谎封IP撒谎覅哈斯；换肤dsa";
-    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:text];
-    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
-    [paragraphStyle setLineSpacing:1.f];//调整行间距
-    [attributedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(0, [text length])];
-    self.jjMessageLabel.attributedText = attributedString;
-    CGSize size = [self.jjMessageLabel sizeThatFits:CGSizeMake(self.replyTable.frame.size.width-40.0, MAXFLOAT)];
-    CGRect frame = self.jjMessageLabel.frame;
-    
-    frame.size.height = size.height;
-    _jjMessageLabel.frame = CGRectMake(20, self.jjLabel.frame.origin.y+35, self.replyTable.frame.size.width-40, frame.size.height);
-    
-   //[self.jjMessageLabel setFrame:frame];
-    
-    self.CenterLineImage.frame = CGRectMake(0,self.jjMessageLabel.frame.size.height+self.jjMessageLabel.frame.origin.y+15 , self.replyTable.frame.size.width, 1);
-    self.topLineImage.hidden = YES;
-    UIImageView * lineImage = [[UIImageView alloc]init];
-    lineImage.frame = CGRectMake(0, self.topLineImage.frame.origin.y, self.replyTable.frame.size.width, 1);
-    lineImage.image = [img resizableImageWithCapInsets:UIEdgeInsetsMake(0 , 1, 0, 1) resizingMode:UIImageResizingModeTile];
-    [self.view addSubview:lineImage];
-    
-    self.yBMRedLine.frame = CGRectMake(14, self.CenterLineImage.frame.origin.y+9, 1, 15);
-    self.yBMLabel.frame = CGRectMake(25, self.CenterLineImage.frame.origin.y+6, 54, 20);
-    self.yBMNumLabel.frame = CGRectMake(87, self.CenterLineImage.frame.origin.y+6, 54, 20);
-    self.checkButton.frame = CGRectMake(self.replyTable.frame.size.width-14-20 , self.CenterLineImage.frame.origin.y+11, 10, 10);
-    self.checkLabel.frame = CGRectMake(self.replyTable.frame.size.width-14-20-5-60, self.CenterLineImage.frame.origin.y+6, 60, 20);
-    self.plbutton.frame = CGRectMake(self.replyTable.frame.size.width-14-50, self.commentView.frame.origin.y+self.commentView.frame.size.height+10, 50, 20);
-    self.zanButton.frame = CGRectMake(self.replyTable.frame.size.width-14-50-50, self.commentView.frame.origin.y+self.commentView.frame.size.height+10, 50, 20);
-    self.bottomLine.frame = CGRectMake(0, self.zanButton.frame.origin.y-6, self.replyTable.frame.size.width, 1);
-    self.viewHeader.height = self.zanButton.frame.origin.y + self.zanButton.frame.size.height +10;
 
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-
 -(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
 {
-    return _viewHeader.height;
+    if ([tableView isEqual:self.replyTable]) {
+        return  self.viewHeader.height;
+    }else if ([tableView isEqual:self.ybmTableView]){
+        
+        return 0;
+    }
+    return 0;
 }
 
 -(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
-    return _viewHeader;
+   
+    if ([tableView isEqual:self.replyTable]) {
+        return  self.viewHeader;
+    }else if ([tableView isEqual:self.ybmTableView]){
+        
+        return nil;
+    }
+    return nil;
+   
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 3;
+    if ([tableView isEqual:self.replyTable]) {
+        section = 3;
+    } else if([tableView isEqual:self.ybmTableView]){
+        section = 3;
+    }
+    return section;
 }
 -(UITableViewCell * )tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *cellIdentifier = @"cellIdentifier";
-    ReplyTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    if (!cell) {
-        cell = [[[NSBundle mainBundle] loadNibNamed:@"ReplyTableViewCell" owner:self options:nil] lastObject];
+    if ([tableView isEqual:self.replyTable]) {
+        NSString *cellIdentifier = @"cellIdentifier";
+        ReplyTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+        if (!cell) {
+            cell = [[[NSBundle mainBundle] loadNibNamed:@"ReplyTableViewCell" owner:self options:nil] lastObject];
+        }
+        cell.backgroundColor = [UIColor redColor];
+        return cell;
+
+    }else if ([tableView isEqual:self.ybmTableView]){
+         NSString *cellIdentifier = @"SHGActionSignTableViewCell";
+        SHGActionSignTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+        if (!cell) {
+            cell = [[[NSBundle mainBundle] loadNibNamed:@"SHGActionSignTableViewCell" owner:self options:nil] lastObject];
+        }
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        return cell;
     }
-    cell.backgroundColor = [UIColor redColor];
-    return cell;
-    
+    return nil;
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    CGFloat rowHeight;
+    if ([tableView isEqual:self.replyTable]) {
+        rowHeight = 44.0;
+    }else if ([tableView isEqual:self.ybmTableView]) {
+        rowHeight = 54.0;
+    }
+    return rowHeight;
 }
 @end
