0f1fa5f1fa611b5648ecac4598073a625f6c812d
魏虔坤 <535138753@qq.com>
2015-11-22 12:47:02 +0800

    添加frends界面

---
+++ Finance/Classes/Circle/Controllers/SHGPersonFriendsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SHGPersonFriendsTableViewCell.h
+//  Finance
+//
+//  Created by 魏虔坤 on 15/11/22.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHGPersonFriendsTableViewCell : UITableViewCell
+
+@end
