cd36f73a1403481a4713eb4b9a7a73061b08f390
魏虔坤 <535138753@qq.com>
2015-11-24 10:05:35 +0800

    添加名片的cell和收藏名片的界面

---
+++ Finance/Classes/Circle/Controllers/SHGCardTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SHGCardTableViewCell.h
+//  Finance
+//
+//  Created by 魏虔坤 on 15/11/24.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHGCardTableViewCell : UITableViewCell
+
+@end
