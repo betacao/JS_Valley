adf4356edd0b1e4df2c78b4819d6c70e509c4941
曹长喜 <941038274@qq.com>
2015-11-26 19:48:06 +0800

    修改完善信息界面崩溃bug

--- Finance/Classes/LoginRegist/ImproveMatiralViewController.m
+++ Finance/Classes/LoginRegist/ImproveMatiralViewController.m
@@ -33,8 +33,7 @@
 @property (weak, nonatomic) IBOutlet UIButton *nextButton;
 @property (weak, nonatomic) IBOutlet UILabel *locationLabel;
 @property (weak, nonatomic) IBOutlet UIButton *manualButton;
-@property (strong,nonatomic) NSString * hangyeCode;
-
+@property (strong, nonatomic) NSString *instustryCode;
 @property (strong, nonatomic) UIImage *headImage;
 @property (strong, nonatomic) NSString *headImageName;
 @property (strong, nonatomic) UITextField *currentField;
@@ -262,34 +261,13 @@
     
 }
 
-- (NSString *)changeIndustrycodeTextFieldText: (NSString *)hangyeCode
-{
-    if ([self.industrycodeTextField.text isEqualToString:@"银行机构"]) {
-       self.hangyeCode = @"bank";
-    }
-    if ([self.industrycodeTextField.text isEqualToString:@"证劵公司"]) {
-        self.hangyeCode = @"bond";
-    }
-    if ([self.industrycodeTextField.text isEqualToString:@"三方理财"]) {
-        self.hangyeCode = @"manage";
-    }
-    if ([self.industrycodeTextField.text isEqualToString:@"基金公司"]) {
-        self.hangyeCode = @"fund";
-    }
-    if ([self.industrycodeTextField.text isEqualToString:@"其他"]) {
-        self.hangyeCode = @"other";
-    }
-    return self.hangyeCode;
-}
-
 - (void)uploadMaterial
 {
     if([self checkInputMessageValid]){
         __weak typeof(self)weakSelf = self;
-        [self changeIndustrycodeTextFieldText:self.hangyeCode];
         NSString *uid = [[NSUserDefaults standardUserDefaults] objectForKey:KEY_UID];
 
-        NSDictionary *param = @{@"uid":uid, @"head_img":self.headImageName ? self.headImageName : @"", @"name":self.nameTextField.text, @"industrycode":self.hangyeCode, @"company":self.companyTextField.text, @"title":self.titleTextField.text, @"position":self.userLocation ? self.userLocation : @""};
+        NSDictionary *param = @{@"uid":uid, @"head_img":self.headImageName ? self.headImageName : @"", @"name":self.nameTextField.text, @"industrycode":self.instustryCode, @"company":self.companyTextField.text, @"title":self.titleTextField.text, @"position":self.userLocation ? self.userLocation : @""};
         [Hud showLoadingWithMessage:@"完善信息中"];
         [[AFHTTPRequestOperationManager manager] PUT:[NSString stringWithFormat:@"%@/%@",rBaseAddressForHttp,@"register"] parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
             NSLog(@"%@",operation);
@@ -529,7 +507,17 @@
 - (void)didSelectIndustry:(NSString *)industry
 {
     self.industrycodeTextField.text = industry;
-    
+    if ([industry isEqualToString:@"银行机构"]) {
+        self.instustryCode = @"bank";
+    } else if ([industry isEqualToString:@"证券公司"]) {
+        self.instustryCode = @"bond";
+    } else if ([industry isEqualToString:@"三方理财"]) {
+        self.instustryCode = @"manage";
+    } else if ([industry isEqualToString:@"基金公司"]) {
+        self.instustryCode = @"fund";
+    } else if ([industry isEqualToString:@"其他"]) {
+        self.instustryCode = @"other";
+    }
     
 }
 
