d562aa2eb8e5da5cc245eb3f760d1032fffe3b5d
魏虔坤 <535138753@qq.com>
2015-12-10 14:09:46 +0800

    添加资讯列表Cell  code review by 小鱼

---
+++ Finance/Classes/Circle/View/SHGNewsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SHGNewsTableViewCell.h
+//  Finance
+//
+//  Created by 魏虔坤 on 15/12/10.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHGNewsTableViewCell : UITableViewCell
+
+@end
---
+++ Finance/Classes/Circle/View/SHGNewsTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SHGNewsTableViewCell.m
+//  Finance
+//
+//  Created by 魏虔坤 on 15/12/10.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import "SHGNewsTableViewCell.h"
+
+@implementation SHGNewsTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
