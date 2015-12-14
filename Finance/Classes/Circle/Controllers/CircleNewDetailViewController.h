1a7ee3f8b173fb890bb6fbdd51ce269b048d1076
魏虔坤 <535138753@qq.com>
2015-12-14 10:28:00 +0800

    添加资讯详情文件

---
+++ Finance/Classes/Circle/Controllers/CircleNewDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  CircleNewDetailViewController.h
+//  Finance
+//
+//  Created by 魏虔坤 on 15/12/14.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+@interface CircleNewDetailViewController : BaseTableViewController
+
+@end
