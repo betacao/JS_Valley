3962141e4de34d7ce5b5df82f1ef50b53d792b31
魏虔坤 <535138753@qq.com>
2015-12-28 16:23:40 +0800

    添加业务二级分类的cell

---
+++ Finance/Classes/Market/SHGMarketSecondCategoryTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SHGMarketSecondCategoryTableViewCell.h
+//  Finance
+//
+//  Created by 魏虔坤 on 15/12/28.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHGMarketSecondCategoryTableViewCell : UITableViewCell
+
+@end
---
+++ Finance/Classes/Market/SHGMarketSecondCategoryTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SHGMarketSecondCategoryTableViewCell.m
+//  Finance
+//
+//  Created by 魏虔坤 on 15/12/28.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import "SHGMarketSecondCategoryTableViewCell.h"
+
+@implementation SHGMarketSecondCategoryTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
