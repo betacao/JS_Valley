3fb796501f37fb618dacdc6dfb578475d8a95743
魏虔坤 <535138753@qq.com>
2015-12-29 14:42:49 +0800

    创建二级分类的自定义button

---
+++ Finance/Classes/Market/SecondCategoryButton.m
@@ -0,0 +1,13 @@
+//
+//  SecondCategoryButton.m
+//  Finance
+//
+//  Created by 魏虔坤 on 15/12/29.
+//  Copyright © 2015年 HuMin. All rights reserved.
+//
+
+#import "SecondCategoryButton.h"
+
+@implementation SecondCategoryButton
+
+@end
