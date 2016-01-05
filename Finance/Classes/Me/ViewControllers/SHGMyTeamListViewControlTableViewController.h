d88d7f362dbbb10c93866464090fe20539e44af3
魏虔坤 <535138753@qq.com>
2016-01-05 10:57:57 +0800

    添加我的合伙人列表

---
+++ Finance/Classes/Me/ViewControllers/SHGMyTeamListViewControlTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHGMyTeamListViewControlTableViewController.h
+//  Finance
+//
+//  Created by 魏虔坤 on 16/1/5.
+//  Copyright © 2016年 HuMin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHGMyTeamListViewControlTableViewController : UITableViewController
+
+@end
