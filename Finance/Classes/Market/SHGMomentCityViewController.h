a713d867fd73c3137274fc9986d7e8f03fa8a275
魏虔坤 <820505758@qq.com>
2016-01-19 19:14:07 +0800

    创建当前城市

---
+++ Finance/Classes/Market/SHGMomentCityViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHGMomentCityViewController.h
+//  Finance
+//
+//  Created by weiqiankun on 16/1/19.
+//  Copyright © 2016年 HuMin. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+@interface SHGMomentCityViewController : BaseTableViewController
+
+@end
---
+++ Finance/Classes/Market/SHGMomentCityViewController.m
@@ -0,0 +1,37 @@
+//
+//  SHGMomentCityViewController.m
+//  Finance
+//
+//  Created by weiqiankun on 16/1/19.
+//  Copyright © 2016年 HuMin. All rights reserved.
+//
+
+#import "SHGMomentCityViewController.h"
+
+@interface SHGMomentCityViewController ()
+
+@end
+
+@implementation SHGMomentCityViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
