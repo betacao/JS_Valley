511e236d2c27b96f5e80461bb6ff2188af2f83d7
曹长喜 <941038274@qq.com>
2016-02-03 15:16:32 +0800

    添加新的搜索界面

---
+++ Finance/Classes/Market/SHGMarketSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHGMarketSearchViewController.h
+//  Finance
+//
+//  Created by changxicao on 16/2/3.
+//  Copyright © 2016年 HuMin. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SHGMarketSearchViewController : BaseViewController
+
+@end
---
+++ Finance/Classes/Market/SHGMarketSearchViewController.m
@@ -0,0 +1,37 @@
+//
+//  SHGMarketSearchViewController.m
+//  Finance
+//
+//  Created by changxicao on 16/2/3.
+//  Copyright © 2016年 HuMin. All rights reserved.
+//
+
+#import "SHGMarketSearchViewController.h"
+
+@interface SHGMarketSearchViewController ()
+
+@end
+
+@implementation SHGMarketSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
