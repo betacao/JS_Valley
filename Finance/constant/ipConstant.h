7a6f060f2765ad83353142021ac4dff468bb1599
曹长喜 <906689960@qq.com>
2016-03-31 10:36:33 +0800

    切换环境

--- Finance/constant/ipConstant.h
+++ Finance/constant/ipConstant.h
@@ -17,9 +17,9 @@
 #define kReleaseH
 
 #ifdef kSitTest
-//#define rBaseAddRessHttp                @"http://120.26.114.154:8080/api"
+#define rBaseAddRessHttp                @"http://120.26.114.154:8080/api"
 //#define rBaseAddRessHttp                @"http://192.168.1.116:8080/api"
-#define rBaseAddRessHttp                @"http://192.168.2.11:8080/api"
+//#define rBaseAddRessHttp                @"http://192.168.2.11:8080/api"
 
 #define rBaseAddressForHttp				[NSString stringWithFormat:@"%@/v2",rBaseAddRessHttp]
 #define rBaseAddressForHttpCircle		[NSString stringWithFormat:@"%@/v2/group",rBaseAddRessHttp]
