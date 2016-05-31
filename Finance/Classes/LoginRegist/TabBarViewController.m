e3ac8af0b4699a01780ab1d8c28288fea3b7a4a4
曹长喜 <906689960@qq.com>
2016-05-31 14:32:20 +0800

    修改转圈位置错误的bug

--- Finance/Classes/LoginRegist/TabBarViewController.m
+++ Finance/Classes/LoginRegist/TabBarViewController.m
@@ -85,8 +85,8 @@
 
 - (void)viewDidAppear:(BOOL)animated
 {
-    [super viewDidAppear:animated];
     self.isViewLoad = YES;
+    [super viewDidAppear:animated];
 }
 
 - (void)viewWillAppear:(BOOL)animated
